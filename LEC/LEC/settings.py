15581286b1a32f9d930e964bf20efa452e31ed77
SetupHub <contact.setuphub@gmail.com>
2020-10-05 05:18:37 -0700

    Static files/media config. Navbar

--- LEC/LEC/settings.py
+++ LEC/LEC/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = '$dv%5+qo4a3sif5w$1=^8&@i9m_p#ml4y3mtu$oui2m0z5l5(3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.177',]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'LEC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'GeneralTemplates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # webLEC
-repositorio para la Web de LEC(Liga Española de Competicion)
+WEB PARA LEC (Liga Española de Competicion)
+
+Pagina principa donde se pueden ver post creados por redactor(Tipo resultado de competiciones,anuncios,info sobre eventos,etc... como si fuera un blog)
+
+Menu para realizar las inscripciones a campeonato:
+  [ ] Para realizar inscripcion primero debe estar registrado(como equipo, no vale nombre de usuario random)
+  [ ] Equipo registra a pilotos(Se le asigna ese equipo al piloto). Cuando piloto haga login se muestra pantalla para poner nombre y contraseña
+  [ ] Primera pantalla donde se elige el campeonato(se muestra si el periodo de inscripcion esta abierto.En caso de no estarlo, se muestra fecha de apertura si existe)
+  [ ] Dentro de campeonato elegido se muestra formulario(diferentes pasos) para inscribir equipo y pilotos.
+
+Consulta de equipos y pilotos por equipos inscritos en campeonatos
+Formulario de contacto con administracion
+Formulario de peticion de traspaso(Si se realiza traspaso de pilotos entre equipos se queda registrado y se muestra en web)
+Se pueda ver streaming oficial desde pagina web
