76d5ed7ade7d5e4f6c0678a0313eb0aad414a904
SetupHub <contact.setuphub@gmail.com>
2020-10-08 04:58:30 -0700

    team register form(no terminado)

--- LEC/usuarios/views.py
+++ LEC/usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import  formulario_registro
+from .forms import  formulario_registro, formulario_registro_equipo
 # Create your views here.
 
 def registro(request):
@@ -22,3 +22,24 @@ def registro(request):
     
 
     return render(request,'GeneralTemplates/registerForm.html', {'form':form})
+
+def registro_equipo(request):
+    """
+    Funcion para registrar los datos del equipo
+    """
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = formulario_registro_equipo(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return render(request,'blog/blog.html')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = formulario_registro_equipo()
+    
+
+    return render(request,'GeneralTemplates/teamRegister.html', {'form':form})
