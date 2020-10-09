782092b4e7b30bb61777cf414b41bad6685e5cf9
SetupHub <contact.setuphub@gmail.com>
2020-10-09 03:50:18 -0700

    create team form and model

---
+++ LEC/blog/static/js/scripts.js
@@ -0,0 +1,20 @@
+function checkform()
+{
+    var f = document.forms["formulario_registro"].elements;
+    var cansubmit = true;
+    let boton = document.getElementById('boton_confirmar');
+
+
+    for (var i = 0; i < f.length; i++) {
+        if (f[i].value.length == 0) cansubmit = false;
+    }
+
+    if (cansubmit) {
+        boton.disabled = false;
+        boton.classList.remove("cursor-not-allowed");
+    }
+    else {
+        boton.disabled = 'disabled';
+
+    }
+}
--- LEC/usuarios/models.py
+++ LEC/usuarios/models.py
@@ -3,13 +3,37 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Equipo(models.Model):
+    """Model definition for Equipo."""
+
+    # TODO: Define fields here
+
+    nombre_equipo = models.CharField( max_length=50)
+    abreviatura = models.CharField(max_length=50)
+    email_equipo = models.EmailField(max_length=254)
+    pilotos = models.ManyToManyField(User, through='Usuario') #foreign key con usuarios
+    activo = models.BooleanField(default = False)
+    fecha_creacion = models.DateTimeField( auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now_add=True)
+    fecha_baja = models.DateTimeField(auto_now_add=False)
+
+    class Meta:
+        """Meta definition for Equipo."""
+        db_table ='equipo'
+        managed = True
+        verbose_name = 'Equipo'
+        verbose_name_plural = 'Equipos'
+
+    def __str__(self):
+        """Unicode representation of Equipo."""
+        return self.nombre_equipo
+
 class Usuario(models.Model):
 
-    user = models.OneToOneField(User, verbose_name="user", on_delete=models.CASCADE)
-    equipo = models.CharField( max_length=50)
-    status = models.CharField( max_length=50)#si es piloto,jefe de  equipo,representante,etc.
+    user = models.OneToOneField(User, verbose_name="usuario", on_delete=models.CASCADE)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    rol = models.CharField( max_length=50) #si es piloto,jefe de  equipo,representante,etc.
     imagen_perfil = models.ImageField( upload_to=None, height_field=None, width_field=None, max_length=None)
-    activo = models.BooleanField()
     registrado = models.DateTimeField( auto_now=False, auto_now_add=False)
     baja = models.DateTimeField( auto_now=False, auto_now_add=False)
 
@@ -24,3 +48,20 @@ class Usuario(models.Model):
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
 
+
+class codigos_activacio_usuarios(models.Model):
+    """Model definition for codigos_activacio_usuarios."""
+
+    # TODO: Define fields here
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) 
+    codigo = models.CharField( max_length=5)
+    class Meta:
+        """Meta definition for codigos_activacio_usuarios."""
+
+        verbose_name = 'Codigo activacion'
+        verbose_name_plural = 'Codigos de activacion'
+
+    def __str__(self):
+        """Unicode representation of codigos_activacio_usuarios."""
+        return self.usuario.id, self.codigo
