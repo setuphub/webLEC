08a64ad21d11346e53eff03c9b5f2e6bf57619d9
SetupHub <contact.setuphub@gmail.com>
2020-10-12 05:54:40 -0700

    login mehtod

--- LEC/blog/urls.py
+++ LEC/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from . import views
-from usuarios.views import registro
+from usuarios.views import registro, login
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('index', views.index, name='index'),
+    path('login', login, name='login')
     #path('registro', registro, name='registro')
 ]
--- LEC/usuarios/views.py
+++ LEC/usuarios/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import  formulario_registro, formulario_registro_equipo
 # Create your views here.
+
+def login(request):
+
+    print(request.POST['nombre_usuario'])
+    username = request.POST['nombre_usuario']
+    password = request.POST['password']
+
+    usuario = authenticate(request, username=username, password=password)
+    print(usuario)
+
+    if usuario is not None:
+        login(request,usuario)
+        return redirect('index')
+    else:
+        return redirect('index') 
 
 def registro(request):
     """
